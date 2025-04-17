0a9b4f35297c0c8aef697a9af9cd8ae2b8609f0d
Git-Mon970 <volodymyrkarabash@gmail.com>
2025-04-17 23:28:55 +0900

    Update Pages

--- payment-web-app/frontend/src/pages/Dashboard.jsx
+++ payment-web-app/frontend/src/pages/Dashboard.jsx
@@ -6,7 +6,6 @@ export const Dashboard = () => {
     return <div>
         <Appbar />
         <div className="m-8">
-            <Balance value={"10,000"} />
             <Users />
         </div>
     </div>
