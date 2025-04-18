7ef55b95f0597c3ec08656354c24db74fb61d017
Git-Mon970 <volodymyrkarabash@gmail.com>
2025-04-19 03:32:46 +0900

    Update Pages

--- payment-web-app/frontend/src/pages/SendMoney.jsx
+++ payment-web-app/frontend/src/pages/SendMoney.jsx
@@ -51,7 +51,6 @@ export const SendMoney = () => {
                             }
                         })
                     }} class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
-                        Initiate Transfer
                     </button>
                 </div>
                 </div>
