0a61e9662ed0f45d57dc5ffdf2d3a06d42995090
Git-Mon970 <volodymyrkarabash@gmail.com>
2025-04-22 21:58:20 +0900

    Update SignIn

--- payment-web-app/frontend/src/pages/Signin.jsx
+++ payment-web-app/frontend/src/pages/Signin.jsx
@@ -13,6 +13,7 @@ export const Signin = () => {
         <InputBox placeholder="azizabdul04327@gmail.com" label={"Email"} />
         <InputBox placeholder="123456" label={"Password"} />
         <div className="pt-4">
+          <Button label={"Sign in"} />
         </div>
         <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
       </div>
